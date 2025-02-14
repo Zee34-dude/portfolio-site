7fe3593feb98f1a0b2b82ca57963f12b8f8678f2
zion ubesie <zionubesie@gmail.com>
2025-02-14 22:33:53 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+new Swiper('.card-wrapper', {
+  loop: true,
+  spaceBetween: 30,
+
+
+  // Pagination bullets
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  //resposive breakpointas
+  breakpoints: {
+    0: {
+      slidesPerView: 1
+    },
+    768: {
+      slidesPerView: 2
+    },
+    1024: {
+      slidesPerView: 3
+    },
+  }
+
+
+});
+
+//sidebar function
+function showSidebar() {
+  const sidebar = document.querySelector('.sidebar')
+  sidebar.style.display = 'flex'
+}
+function closeSidebar()  {
+  const sidebar = document.querySelector('.sidebar')
+  sidebar.style.display = 'none'
+}
